c6c176b94e46365f09771a27a8412a06f9772c34
DinaKSari <dinakumala7070@gmail.com>
2025-04-15 08:53:41 +0700

    menambahkan variable dan konstruktor berparameter di matakuliah

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,3 +1,11 @@
 public class MataKuliah{
-    
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    MataKuliah(String kodeMK, String namaMK, int sks){
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
 }
