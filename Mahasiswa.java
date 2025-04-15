bc15f2914852eeb268d33ab516e582e55bbd3fd4
DinaKSari <dinakumala7070@gmail.com>
2025-04-15 09:39:09 +0700

    menambah method tampilkan mahasiswa dengan array index

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -3,6 +3,7 @@ public class Mahasiswa {
     String nama;
     String prodi;
     Mahasiswa dataMahasiswa[] = new Mahasiswa[5];
+    int idx;
 
     //KONSTRUKTOR BERPARAMETER
     Mahasiswa(String nim, String nama, String prodi){
@@ -14,7 +15,24 @@ public class Mahasiswa {
     Mahasiswa(){
     }
 
-void tampilMahasiswa(){
+    void tampilMahasiswa(){
         System.out.println("NIM: " + nim + " | Nama: " + nama + " | Prodi " + prodi);
+    }
+
+    void tambah(Mahasiswa mhs) {
+        if (idx < dataMahasiswa.length) {
+            dataMahasiswa[idx] = mhs;
+            idx++;
+        } else {
+            System.out.println("Data mahasiswa sudah penuh!");
+        }
+    }
+
+    void tampilMhs(){
+        for (int i = 0; i < idx; i++) { 
+            if (dataMahasiswa[i] != null) {
+                dataMahasiswa[i].tampilMhs();
+            }
+        }
     }
 }
